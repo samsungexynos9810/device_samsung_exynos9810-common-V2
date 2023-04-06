6724e046c37cf3761c184510cfc3da3d1dd4c18f
Dylan Neve <dylanneve1@gmail.com>
2023-04-07 00:31:48 +0100

    exynos9810-common: sthal: initial adaptation
    Correct LOCAL_MODULE name and includes path. Also removed STHAL_TEST for vts testing and forced LOCAL_PROPRIETARY_MODULE.

--- sthal/Android.mk
+++ sthal/Android.mk
@@ -17,20 +17,16 @@
 #
 # Primary SoundTrigger HAL module
 #
-ifeq ($(BOARD_USE_SOUNDTRIGGER_HAL),true)
 LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
-LOCAL_PROPRIETARY_MODULE := true
-
-DEVICE_BASE_PATH := $(TOP)/device/samsung
 
 LOCAL_SRC_FILES := \
         sound_trigger_hw.c
 
 LOCAL_C_INCLUDES += \
         external/tinyalsa/include \
-	$(DEVICE_BASE_PATH)/$(TARGET_BOOTLOADER_BOARD_NAME)/conf
+	$(COMMON_PATH)/configs/audio
 
 LOCAL_SHARED_LIBRARIES := \
         liblog libcutils libtinyalsa libhardware
@@ -39,21 +35,10 @@ ifeq ($(BOARD_USE_SOUNDTRIGGER_HAL_MMAP),true)
 LOCAL_CFLAGS += -DMMAP_INTERFACE_ENABLED
 endif
 
-LOCAL_MODULE := sound_trigger.primary.$(TARGET_SOC)
+LOCAL_MODULE := sound_trigger.primary.exynos9810
 
-ifeq ($(BOARD_USES_VENDORIMAGE), true)
 LOCAL_PROPRIETARY_MODULE := true
-endif
 LOCAL_MODULE_RELATIVE_PATH := hw
 LOCAL_MODULE_TAGS := optional
 
 include $(BUILD_SHARED_LIBRARY)
-
-else
-
-ifeq ($(BOARD_USE_SOUNDTRIGGER_HAL), STHAL_TEST)
-LOCAL_PATH := $(call my-dir)
-include $(call all-makefiles-under,$(LOCAL_PATH))
-endif
-
-endif
